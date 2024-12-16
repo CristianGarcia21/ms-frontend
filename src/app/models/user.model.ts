01e52c1a92f534e9e22f498ee03d79cc1953bde3
edissonRamirez <edisson.ramirez29529@ucaldas.edu.co>
2024-12-15 20:16:35 -0500

    Creación de controlador y vista de Login

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,7 +1,10 @@
+import { Role } from './role.model';
+
 export class User {
   id?:number
   user_id?:string
-  username:string
+  username?:string
   email:string
   password:string
+  role?: Role
 }
