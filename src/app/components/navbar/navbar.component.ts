664c8df9cfed32f26c683496b9f309660d64212f
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-12-17 21:02:57 -0500

    Logout y mostrar nombre de usuario

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,36 +1,66 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
-import { ROUTES } from '../sidebar/sidebar.component';
-import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
-import { Router } from '@angular/router';
+import { Component, OnInit, ElementRef } from "@angular/core";
+import { ROUTES } from "../sidebar/sidebar.component";
+import {
+  Location,
+  LocationStrategy,
+  PathLocationStrategy,
+} from "@angular/common";
+import { Router } from "@angular/router";
+import { SecurityService } from "src/app/services/security.service";
+import { WebSocketService } from "src/app/services/web-socket.service";
+import { User } from "src/app/models/user.model";
+import { Subscription } from "rxjs";
+import { log } from "console";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent implements OnInit {
   public focus;
   public listTitles: any[];
   public location: Location;
-  constructor(location: Location,  private element: ElementRef, private router: Router) {
+  user: User;
+  subscription: Subscription;
+  constructor(
+    location: Location,
+    private element: ElementRef,
+    private router: Router,
+    private securityService: SecurityService,
+    private webSocketService: WebSocketService
+  ) {
     this.location = location;
+    this.subscription = this.securityService.getUser().subscribe((data) => {
+      this.user = data;
+      //this.webSocketService.setNameEvent(this.user.email) Para el chat
+      console.log(this.user);
+    });
   }
 
   ngOnInit() {
-    this.listTitles = ROUTES.filter(listTitle => listTitle);
+    this.listTitles = ROUTES.filter((listTitle) => listTitle);
   }
-  getTitle(){
+  getTitle() {
     var titlee = this.location.prepareExternalUrl(this.location.path());
-    if(titlee.charAt(0) === '#'){
-        titlee = titlee.slice( 1 );
+    if (titlee.charAt(0) === "#") {
+      titlee = titlee.slice(1);
     }
 
-    for(var item = 0; item < this.listTitles.length; item++){
-        if(this.listTitles[item].path === titlee){
-            return this.listTitles[item].title;
-        }
+    for (var item = 0; item < this.listTitles.length; item++) {
+      if (this.listTitles[item].path === titlee) {
+        return this.listTitles[item].title;
+      }
     }
-    return 'Dashboard';
+    return "Dashboard";
   }
 
+  public getSession() {
+    return this.securityService.existSession();
+  }
+  logout() {
+    this.securityService.logout();
+    this.router.navigate(["/login"]);
+    this.webSocketService.disconnect()
+  }
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -3,6 +3,7 @@ import { Role } from './role.model';
 export class User {
   id?:number
   user_id?:string
+  name?:string
   username?:string
   email:string
   password:string
